e603dc65ba85dd388812d1672ef9d1a2e9f10d8d
Abdullah Alyan <AbdullahAlyan@yahoo.com>
2024-01-11 10:18:51 +0300

    feat: support mender-partuuid feature
    Signed-off-by: Abdullah Alyan <AbdullahAlyan@yahoo.com>

--- classes/mender-kernel-helpers.bbclass
+++ classes/mender-kernel-helpers.bbclass
@@ -4,8 +4,13 @@ mender_kernel_delete_kernel_parts() {
     bbfatal "mender_kernel_delete_kernel_parts()::file($1) is not valid or does not exist"
   fi
 
-  local kernelparta="${MENDER/KERNEL_PART_A}"
-  local kernelpartb="${MENDER/KERNEL_PART_B}"
+  if ${@bb.utils.contains('MENDER_FEATURES', 'mender-partuuid', 'true', 'false', d)}; then
+    local kernelparta="PARTUUID=${@os.path.basename(d.getVar('MENDER/KERNEL_PART_A'))}"
+    local kernelpartb="PARTUUID=${@os.path.basename(d.getVar('MENDER/KERNEL_PART_B'))}"
+  else
+    local kernelparta="${MENDER/KERNEL_PART_A}"
+    local kernelpartb="${MENDER/KERNEL_PART_B}"
+  fi
 
   sed -i -e "\|${kernelparta}|d" \
          -e "\|${kernelpartb}|d" \
--- recipes-mender/mender-kernel-state-scripts/files/write-kernel-part.sh
+++ recipes-mender/mender-kernel-state-scripts/files/write-kernel-part.sh
@@ -38,46 +38,46 @@ ROOT_MNT_DIR="@@MENDER/KERNEL_ROOT_CANDIDATE_MNT_DIR@@"
 if   [ "$BOOT_PART" -eq "@@MENDER_ROOTFS_PART_A_NUMBER@@" ] && [ "$UPGRADE_AV" -ne "0" ]; then
   # an update has already happened, but the system hasn't restarted. mender is now overwriting it.
   # don't need to invert the reported mender_boot_part
-  ROOT_PART="@@MENDER_ROOTFS_PART_A_NUMBER@@"
-  KERN_PART="@@MENDER/KERNEL_PART_A_NUMBER@@"
+  ROOT_PART="@@MENDER_ROOTFS_PART_A@@"
+  KERN_PART="@@MENDER/KERNEL_PART_A@@"
 
 elif [ "$BOOT_PART" -eq "@@MENDER_ROOTFS_PART_A_NUMBER@@" ]; then
   # invert mender_boot_part to mount the update candidate partitions
-  ROOT_PART="@@MENDER_ROOTFS_PART_B_NUMBER@@"
-  KERN_PART="@@MENDER/KERNEL_PART_B_NUMBER@@"
+  ROOT_PART="@@MENDER_ROOTFS_PART_B@@"
+  KERN_PART="@@MENDER/KERNEL_PART_B@@"
 
 elif [ "$BOOT_PART" -eq "@@MENDER_ROOTFS_PART_B_NUMBER@@" ] && [ "$UPGRADE_AV" -ne "0" ]; then
   # an update has already happened, but the system hasn't restarted. mender is now overwriting it.
   # don't need to invert the reported mender_boot_part
-  ROOT_PART="@@MENDER_ROOTFS_PART_B_NUMBER@@"
-  KERN_PART="@@MENDER/KERNEL_PART_B_NUMBER@@"
+  ROOT_PART="@@MENDER_ROOTFS_PART_B@@"
+  KERN_PART="@@MENDER/KERNEL_PART_B@@"
 
 elif [ "$BOOT_PART" -eq "@@MENDER_ROOTFS_PART_B_NUMBER@@" ]; then
   # invert mender_boot_part to mount the update candidate partitions
-  ROOT_PART="@@MENDER_ROOTFS_PART_A_NUMBER@@"
-  KERN_PART="@@MENDER/KERNEL_PART_A_NUMBER@@"
+  ROOT_PART="@@MENDER_ROOTFS_PART_A@@"
+  KERN_PART="@@MENDER/KERNEL_PART_A@@"
 
 else
-  fatal "@@MENDER_STORAGE_DEVICE_BASE@@$BOOT_PART is not a known rootfs partition"
+  fatal "invalid or unknown rootfs partition: $BOOT_PART"
 
 fi
 
-log "updating @@MENDER_STORAGE_DEVICE_BASE@@$KERN_PART with new kernel candidate from @@MENDER_STORAGE_DEVICE_BASE@@$ROOT_PART"
+log "updating $KERN_PART with new kernel candidate from $ROOT_PART"
 
 if ! mount |                                      grep -q $ROOT_MNT_DIR; then
   mkdir -p                                                $ROOT_MNT_DIR
-  mount -o ro  @@MENDER_STORAGE_DEVICE_BASE@@$ROOT_PART   $ROOT_MNT_DIR
-  log "mounted @@MENDER_STORAGE_DEVICE_BASE@@$ROOT_PART @ $ROOT_MNT_DIR"
+  mount -o ro  $ROOT_PART   $ROOT_MNT_DIR
+  log "mounted $ROOT_PART @ $ROOT_MNT_DIR"
 fi
 
 if ! mount |                                      grep -q $KERN_MNT_DIR; then
   mkdir -p                                                $KERN_MNT_DIR
-  mount -o rw  @@MENDER_STORAGE_DEVICE_BASE@@$KERN_PART   $KERN_MNT_DIR
-  log "mounted @@MENDER_STORAGE_DEVICE_BASE@@$KERN_PART @ $KERN_MNT_DIR"
+  mount -o rw  $KERN_PART   $KERN_MNT_DIR
+  log "mounted $KERN_PART @ $KERN_MNT_DIR"
 fi
 
 rsync -avqI $ROOT_MNT_DIR/$KERN_SRC_DIR/*                 $KERN_MNT_DIR --exclude $(basename @@MENDER_BOOT_PART_MOUNT_LOCATION@@)
 
-log "finished updating kernel partition: @@MENDER_STORAGE_DEVICE_BASE@@$KERN_PART"
+log "finished updating kernel partition: $KERN_PART"
 
 exit
