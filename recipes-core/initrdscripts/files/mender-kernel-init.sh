516562f5afea780b5cf3b712bb4c5cf7e4c4bacd
Corey Cothrum <contact@coreycothrum.com>
2024-10-11 13:02:18 +0000

    fix(initrdscripts): mknod should be inside init script

--- recipes-core/initrdscripts/files/mender-kernel-init.sh
+++ recipes-core/initrdscripts/files/mender-kernel-init.sh
@@ -2,30 +2,19 @@
 ################################################################################
 PATH=$PATH:/sbin:/bin:/usr/sbin:/usr/bin
 
-mkdir -p /proc ; mount -n -t proc     proc     /proc
-mkdir -p /sys  ; mount -n -t sysfs    sysfs    /sys
 mkdir -p /dev  ; mount -n -t devtmpfs devtmpfs /dev
+mkdir -p /proc ; mount -n -t proc     proc     /proc
 mkdir -p /run  ; mount -n -t tmpfs    tmpfs    /run
+mkdir -p /sys  ; mount -n -t sysfs    sysfs    /sys
 
 mknod /dev/console c 5 1
 mknod /dev/null    c 1 3
 mknod /dev/zero    c 1 5
 
 ################################################################################
-CONSOLE="/dev/console"
-
-MNT_DIR="/tmp"
-
-ROOT_MNT="$MNT_DIR/root"
 ROOT_DEV=""
+ROOT_MNT="/tmp/root"
 
-BOOT_MNT="$MNT_DIR@@MENDER_BOOT_PART_MOUNT_LOCATION@@"
-BOOT_DEV=@@MENDER_BOOT_PART@@
-
-DATA_MNT="$MNT_DIR@@MENDER_DATA_PART_MOUNT_LOCATION@@"
-DATA_DEV=@@MENDER_DATA_PART@@
-
-################################################################################
 read_args() {
   [ -z "${CMDLINE+x}" ] && CMDLINE=`cat /proc/cmdline`
   for arg in $CMDLINE; do
@@ -40,11 +29,10 @@ read_args() {
 ################################################################################
 read_args
 
-mkdir  -p           $BOOT_MNT
-mount     $BOOT_DEV $BOOT_MNT
+mkdir  -p                      @@MENDER_BOOT_PART_MOUNT_LOCATION@@
+mount     @@MENDER_BOOT_PART@@ @@MENDER_BOOT_PART_MOUNT_LOCATION@@
 
 mkdir  -p           $ROOT_MNT
 mount     $ROOT_DEV $ROOT_MNT
-
 cd                  $ROOT_MNT
 exec switch_root    $ROOT_MNT /sbin/init
--- recipes-core/initrdscripts/mender-kernel-init-script_latest.bb
+++ recipes-core/initrdscripts/mender-kernel-init-script_latest.bb
@@ -19,12 +19,7 @@ RDEPENDS:${PN}    = "                   \
 inherit bitbake-variable-substitution
 
 do_install () {
-    install -d      ${D}/dev
-    mknod   -m 0600 ${D}/dev/console c 5 1
-    mknod   -m 0600 ${D}/dev/null    c 1 3
-    mknod   -m 0600 ${D}/dev/zero    c 1 5
-
-    install -m 0755 ${WORKDIR}/mender-kernel-init.sh ${D}/init
+  install -m 0755 ${WORKDIR}/mender-kernel-init.sh ${D}/init
 }
 
 inherit allarch
